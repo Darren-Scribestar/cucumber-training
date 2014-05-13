c0c1b7efb8926575334dc2625bf7f3d66d4ab19d
Darren Bown <darren.bown@scribestar.com>
2014-05-13 08:56:29 +0100

    Removed unused step definitions

--- stories/features/step_definitions/example_steps.rb
+++ stories/features/step_definitions/example_steps.rb
@@ -12,27 +12,15 @@ Then(/^I should see "IANA"/) do
   @browser.close
 end
 
-
 Given(/^I am on the w3schools forms page$/) do
   @browser = Watir::Browser.new
   @browser.goto "http://www.w3schools.com/html/html_forms.asp"
-end
-
-When(/^I add a username$/) do
-  @browser.text_field(:name=> 'user').set "A username"
 end
 
 When(/^I submit the form$/) do
   @browser.text_field(:name=> 'user').set "A username"
   @browser.button(:value=>'Submit').click
 end
-
-Then(/^I see the username in the field$/) do
-  $stdout.puts @browser.text_field(:name=> 'user').value
-  raise "Form not filled" unless @browser.text_field(:name=> 'user').value == "A username"
-  @browser.close
-end
-
 
 Then(/^I see the username displayed on the next page$/) do
   @browser.window(:title => /Forms action page/).use do
